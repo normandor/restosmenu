ef34c269e7efe0f3491ce62f08af25473539aba2
Norman Riobo <n.riobo@napoleongames.be>
2020-10-06 06:50:45 +0200

    Add getters and setters to PageVisits

--- src/Entity/PageVisits.php
+++ src/Entity/PageVisits.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AppBundle;
+namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
@@ -49,5 +49,113 @@ class PageVisits
      */
     private $cityRegion;
 
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="restaurant_id", type="integer", nullable=true)
+     */
+    private $restaurantId;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="referer", type="string", length=255, nullable=false)
+     */
+    private $referer;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="agent", type="string", length=255, nullable=false)
+     */
+    private $agent;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIp(): ?string
+    {
+        return $this->ip;
+    }
+
+    public function setIp(string $ip): self
+    {
+        $this->ip = $ip;
+
+        return $this;
+    }
+
+    public function getDatetime(): ?\DateTimeInterface
+    {
+        return $this->datetime;
+    }
+
+    public function setDatetime(?\DateTimeInterface $datetime): self
+    {
+        $this->datetime = $datetime;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getCityRegion(): ?string
+    {
+        return $this->cityRegion;
+    }
+
+    public function setCityRegion(string $cityRegion): self
+    {
+        $this->cityRegion = $cityRegion;
+
+        return $this;
+    }
+
+    public function getRestaurantId(): ?int
+    {
+        return $this->restaurantId;
+    }
+
+    public function setRestaurantId(?int $restaurantId): self
+    {
+        $this->restaurantId = $restaurantId;
+
+        return $this;
+    }
+
+    public function getReferer(): ?string
+    {
+        return $this->referer;
+    }
+
+    public function setReferer(string $referer): self
+    {
+        $this->referer = $referer;
+
+        return $this;
+    }
+
+    public function getAgent(): ?string
+    {
+        return $this->agent;
+    }
+
+    public function setAgent(string $agent): self
+    {
+        $this->agent = $agent;
+
+        return $this;
+    }
 }
