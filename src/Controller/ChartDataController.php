d78fa09d4f280718b88ba253b8aadb32b351d206
Norman Riobo <n.riobo@napoleongames.be>
2020-10-12 22:44:14 +0200

    Add graphs by day

--- src/Controller/ChartDataController.php
+++ src/Controller/ChartDataController.php
@@ -9,6 +9,9 @@ use App\Service\ChartsService;
 
 class ChartDataController extends AbstractController
 {
+    const UNIT_MES = 'm';
+    const UNIT_DAY = 'd';
+
     public function index($id)
     {
         return new Response('Unrecognized call');
@@ -16,7 +19,14 @@ class ChartDataController extends AbstractController
 
     public function getLineTotalDataForRestaurant(ChartsService $chartsService)
     {
-        $json = $chartsService->getLineTotalDataForRestaurant($this->getUser()->getRestaurantId());
+        $json = $chartsService->getLineTotalDataForRestaurant($this->getUser()->getRestaurantId(), self::UNIT_MES);
+
+        return new Response($json);
+    }
+
+    public function getLineTotalDataForRestaurantByDay(ChartsService $chartsService)
+    {
+        $json = $chartsService->getLineTotalDataForRestaurant($this->getUser()->getRestaurantId(), self::UNIT_DAY);
 
         return new Response($json);
     }
@@ -27,7 +37,18 @@ class ChartDataController extends AbstractController
             return new Response([]);
         }
 
-        $json = $chartsService->getLineTotalData();
+        $json = $chartsService->getLineTotalData(self::UNIT_MES);
+
+        return new Response($json);
+    }
+
+    public function getLineTotalDataByDay(ChartsService $chartsService)
+    {
+        if (!in_array('ROLE_VIEW_GRAPHS', $this->getUser()->getRoles(), true)) {
+            return new Response([]);
+        }
+
+        $json = $chartsService->getLineTotalData(self::UNIT_DAY);
 
         return new Response($json);
     }
@@ -38,7 +59,18 @@ class ChartDataController extends AbstractController
             return new Response([]);
         }
 
-        $json = $chartsService->getLineDataPerRestaurant();
+        $json = $chartsService->getLineDataPerRestaurant(self::UNIT_MES);
+
+        return new Response($json);
+    }
+
+    public function getLineDataPerRestaurantByDay(ChartsService $chartsService)
+    {
+        if (!in_array('ROLE_VIEW_GRAPHS', $this->getUser()->getRoles(), true)) {
+            return new Response([]);
+        }
+
+        $json = $chartsService->getLineDataPerRestaurant(self::UNIT_DAY);
 
         return new Response($json);
     }
--- src/Service/ChartsService.php
+++ src/Service/ChartsService.php
@@ -2,12 +2,16 @@
 
 namespace App\Service;
 
+use App\Controller\ChartDataController;
 use App\Entity\PageVisits;
+use DateInterval;
+use DatePeriod;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 
+// TODO: remove duplicate code in these calls
 class ChartsService
 {
-
     private $entityManager;
 
     public function __construct(EntityManagerInterface $entityManager)
@@ -16,23 +20,38 @@ class ChartsService
     }
 
     /**
+     * @param string $unit
+     *
      * @return json
      * @throws \Doctrine\DBAL\DBALException
      */
-    public function getLineTotalData()
+    public function getLineTotalData(string $unit)
     {
         $conn = $this->entityManager->getConnection();
 
         $yAxisTitleTot = 'Visitas';
         $seriesNameTot = 'Global';
-        //        $seriesColorTot = ['#C0392B', '#8E44AD', '#2E86C1', '#17A589', '#D4AC0D', '#CA6F1E', '#839192'];
         $seriesColorTot = '#CA6F1E';
         $seriesAreaColorTot = '#FAD7A0';
         $dataLegendTot = 'Global';
-        $dateArrayTot = $this->getMonthsXAxis();
+
+        if (ChartDataController::UNIT_MES === $unit) {
+            $dateArrayTot = $this->getMonthsXAxis();
+        } else {
+            $today = new \DateTime();
+            $tomorrow = new \DateTime();
+            $tomorrow->modify('-6 months');
+
+            $dateArrayTot = $this->getDatesFromRange($tomorrow->format('Y-m-d'), $today->format('Y-m-d'));
+        }
         $dataRow = [];
         for ($i = 0, $iMax = count($dateArrayTot); $i < $iMax; $i++) {
-            $query = "SELECT COUNT(*) as cnt FROM page_visits WHERE LEFT(datetime,7) = :laDate";
+            if (ChartDataController::UNIT_MES === $unit) {
+                $query = 'SELECT COUNT(*) as cnt FROM page_visits WHERE LEFT(datetime,7) = :laDate';
+            } else {
+                $query = 'SELECT COUNT(*) as cnt FROM page_visits WHERE LEFT(datetime,10) = :laDate';
+            }
+
             $stmt = $conn->prepare($query);
             $stmt->execute(['laDate' => $dateArrayTot[$i]]);
             $stmt->execute();
@@ -52,10 +71,13 @@ class ChartsService
     }
 
     /**
+     * @param int    $restaurantId
+     * @param string $unit
+     *
      * @return json
      * @throws \Doctrine\DBAL\DBALException
      */
-    public function getLineTotalDataForRestaurant($restaurantId)
+    public function getLineTotalDataForRestaurant($restaurantId, $unit)
     {
         $conn = $this->entityManager->getConnection();
 
@@ -64,10 +86,25 @@ class ChartsService
         $seriesColorTot = '#CA6F1E';
         $seriesAreaColorTot = '#FAD7A0';
         $dataLegendTot = 'Global';
-        $dateArrayTot = $this->getMonthsXAxis();
+
+        if (ChartDataController::UNIT_MES === $unit) {
+            $dateArrayTot = $this->getMonthsXAxis();
+        } else {
+            $today = new \DateTime();
+            $tomorrow = new \DateTime();
+            $tomorrow->modify('-6 months');
+
+            $dateArrayTot = $this->getDatesFromRange($tomorrow->format('Y-m-d'), $today->format('Y-m-d'));
+        }
+
         $dataRow = [];
         for ($i = 0, $iMax = count($dateArrayTot); $i < $iMax; $i++) {
-            $query = "SELECT COUNT(*) as cnt FROM page_visits WHERE LEFT(datetime,7) = :laDate AND restaurant_id = :restaurant_id";
+            if (ChartDataController::UNIT_MES === $unit) {
+                $query = 'SELECT COUNT(*) as cnt FROM page_visits WHERE LEFT(datetime,7) = :laDate AND restaurant_id = :restaurant_id';
+            } else {
+                $query = 'SELECT COUNT(*) as cnt FROM page_visits WHERE LEFT(datetime,10) = :laDate AND restaurant_id = :restaurant_id';
+            }
+
             $stmt = $conn->prepare($query);
             $stmt->execute(['laDate' => $dateArrayTot[$i], 'restaurant_id' => $restaurantId]);
             $stmt->execute();
@@ -85,17 +122,28 @@ class ChartsService
             'seriesDataTot' => $dataRow,
         ]);
     }
+
     /**
+     * @param string $unit
+     *
      * @return json
      */
-    public function getLineDataPerRestaurant()
+    public function getLineDataPerRestaurant($unit)
     {
         $yAxisTitleTot = 'Visitas';
         $seriesNames = [];
         $seriesColorTot = ['#C0392B', '#8E44AD', '#2E86C1', '#17A589', '#D4AC0D', '#CA6F1E', '#839192'];
         $seriesAreaColorTot = ['#C0392B', '#8E44AD', '#2E86C1', '#17A589', '#D4AC0D', '#CA6F1E', '#839192'];
         $dataLegendTot = 'Global';
-        $dateArrayTot = $this->getMonthsXAxis();
+        if (ChartDataController::UNIT_MES === $unit) {
+            $dateArrayTot = $this->getMonthsXAxis();
+        } else {
+            $today = new \DateTime();
+            $tomorrow = new \DateTime();
+            $tomorrow->modify('-6 months');
+
+            $dateArrayTot = $this->getDatesFromRange($tomorrow->format('Y-m-d'), $today->format('Y-m-d'));
+        }
 
         $pageVisitsRepository = $this->entityManager->getRepository(PageVisits::class);
         $restaurantsArray = $pageVisitsRepository->getAllRestaurants();
@@ -105,7 +153,11 @@ class ChartsService
             $seriesNames[] = $restaurant['name'];
             $dataRow = [];
             foreach ($dateArrayTot as $yearMonth) {
-                $dataRow[] = $pageVisitsRepository->getCountByRestaurantIdAndYearMonth($restaurant['id'], $yearMonth);
+                if (ChartDataController::UNIT_MES === $unit) {
+                    $dataRow[] = $pageVisitsRepository->getCountByRestaurantIdAndYearMonth($restaurant['id'], $yearMonth);
+                } else {
+                    $dataRow[] = $pageVisitsRepository->getCountByRestaurantIdAndDay($restaurant['id'], $yearMonth);
+                }
             }
 
             $dataRowArray[] = $dataRow;
@@ -145,5 +197,31 @@ class ChartsService
         }
 
         return array_reverse($months);
+    }
+
+    /**
+     * @param        $start
+     * @param        $end
+     * @param string $format
+     *
+     * @return array
+     * @throws \Exception
+     */
+    function getDatesFromRange($start, $end, $format = 'Y-m-d')
+    {
+        $array = array();
+
+        $interval = new DateInterval('P1D');
+
+        $realEnd = new DateTime($end);
+        $realEnd->add($interval);
+
+        $period = new DatePeriod(new DateTime($start), $interval, $realEnd);
+
+        foreach($period as $date) {
+            $array[] = $date->format($format);
+        }
+
+        return $array;
     }
 }
